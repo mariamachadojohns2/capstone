2b2fa89be78a028267334f75073212e3c2e27853
Maria Johnson <mariamachadojohns2@gmail.com>
2026-05-29 18:48:17 -0400

    Update qualitycontrol.sh

--- qualitycontrol.sh
+++ qualitycontrol.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# Download FastQ sample files
+mkdir -p data/untrimmed_fastq
+cd data/untrimmed_fastq
+
+curl -O ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR258/004/SRR2589044/SRR2589044_1.fastq.gz
+curl -O ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR258/004/SRR2589044/SRR2589044_2.fastq.gz
+curl -O ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR258/003/SRR2584863/SRR2584863_1.fastq.gz
+curl -O ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR258/003/SRR2584863/SRR2584863_2.fastq.gz
+curl -O ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR258/006/SRR2584866/SRR2584866_1.fastq.gz
+curl -O ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR258/006/SRR2584866/SRR2584866_2.fastq.gz
+
 # Perform basic quality control on FASTQ files
 
 # Run FASTQC on untrimmed reads
